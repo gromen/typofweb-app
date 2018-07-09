64ce0328077779296c2deaed11edf2d70a67d54a
Marcin Gromek <gromson101@gmail.com>
2018-07-10 00:56:57 +0200

    create components

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AppHeader = (props) => (
+    <header className="App-header">
+        <img src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+    </header>
+);
+
+export default AppHeader;
---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ContactItem from './ContactItem/ContactItem';
+
+const ContactsList = () => (
+    <ContactItem />
+);
+
+export default ContactsList;
